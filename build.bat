8adce1dc76b7c0bac30f36e256a1c626a9345b78
pixelsuft <yuskivl07@mail.ru>
2021-10-24 17:57:27 +0700

    updates

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 title building...
 color 0a
-pyinstaller -F -w -i files\icon.ico --add-data "files;files" main.py
+pyinstaller -F -w -i files\icon.ico --uac-admin --add-data "files;files" main.py
 echo ok
 pause
